adf97f2e6d857c99f1b75e2f83a8299d36b16897
kunavich <Kunpash@gmail.com>
2023-11-09 01:54:36 +0100

    insert value

---
+++ src/main/resources/db/migration/V3__first_value_for_exchange_rate.sql
@@ -0,0 +1,9 @@
+INSERT INTO exchange_rate (symbol, rate, rate_timestamp)
+VALUES ('USD/KZT', 0.0, '18-06-12 10:34:09 AM');
+
+INSERT INTO exchange_rate (symbol, rate, rate_timestamp)
+VALUES ('USD/RUB', 0.0, '18-06-12 10:34:09 AM');
+
+
+
+
---
+++ src/main/resources/db/migration/V4__update_value_for_exchange_rate.sql
@@ -0,0 +1,6 @@
+UPDATE exchange_rate
+SET rate_timestamp='2018-11-12 01:02:03.123456789';
+
+
+
+
