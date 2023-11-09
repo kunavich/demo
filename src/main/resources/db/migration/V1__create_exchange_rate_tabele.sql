ce5fec3ad1d7f5b87d1810df342af00c531a4d12
kunavich <Kunpash@gmail.com>
2023-11-09 01:55:21 +0100

    insert value

--- src/main/resources/db/migration/V1__create_exchange_rate_tabele.sql
+++ src/main/resources/db/migration/V1__create_exchange_rate_tabele.sql
@@ -2,5 +2,5 @@ CREATE TABLE exchange_rate (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   symbol varchar(45) NOT NULL,
   rate varchar(45) NOT NULL,
-  rate_timestamp timestamp NOT NULL
+  rate_timestamp varchar(45) NOT NULL
 );
